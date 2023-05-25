7249fe1afa36391d32ac6e204523151ce5fa4e10
hudsonhrh <hudsonhrh7@gmail.com>
2023-05-25 17:08:37 -0500

    made script to test agents with human

--- dai/src/scripts/decent.py
+++ dai/src/scripts/decent.py
@@ -1,4 +1,7 @@
+from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
+from langchain.agents import load_tools, initialize_agent
+from langchain.agents import AgentType
 import os
 
 # Use os.environ to get environment variables
@@ -7,16 +10,19 @@ if not openai_api_key:
     raise ValueError("Missing environment variable: 'NEXT_PUBLIC_OPENAI'")
 
 
-llm = OpenAI(openai_api_key=openai_api_key, temperature=.9)
 
-
-from langchain.prompts import PromptTemplate
-
-prompt = PromptTemplate(
-    input_variables=["product"],
-    template="What is a good name for a company that makes {product}?",
+llm = ChatOpenAI(openai_api_key=openai_api_key, temperature=0.0)
+math_llm = OpenAI(openai_api_key=openai_api_key,temperature=0.0)
+tools = load_tools(
+    ["human", "llm-math"], 
+    llm=math_llm,
 )
 
+agent_chain = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+)
 
-
-print(llm(prompt.format(product="DAO AI tools")))
+agent_chain.run("What's my friend Eric's surname?")
